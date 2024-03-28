9bf8b8ab4b2ee1b529c345d31f272c3947f343e1
sandeep jain <mr.sandeepmcscet@gmail.com>
2024-03-28 20:44:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('button')
+button.addEventListener('click',(e)=>{
+e.preventDefault();
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value)
+const results = document.querySelector('#results')
+if(height === '' || height<=0 || isNaN(height)){
+    console.log('at height');
+    results.innerHTML = `Please enter valid height ${height}`
+}else if(weight === '' || weight<=0 || isNaN(weight)){
+    results.innerHTML = `Please enter valid weight ${weight}`
+} else{
+    const bmi = (weight/((height*height)/10000)).toFixed(2)
+   
+    if(bmi<=18.6){
+        $msg= 'Under Weight';
+    }else if( bmi >= 24.9){
+        $msg= 'over Range';
+    } else {
+        $msg= 'Normal Range';
+    }
+    results.innerHTML = `your bmi is ${bmi} and you are ${$msg}`;
+
+}
+
+})
